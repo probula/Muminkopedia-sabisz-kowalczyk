a9b9ebf889332a7537c1f14b0dca3aa4bd8d9f20
Joseph <blazej.sabisz@cyfroweszkoly.pl>
2026-04-08 19:05:43 +0200

    stworzenie polaczenia z baza

---
+++ src/bin/db.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const connectDB = async (): Promise<void> => {
+    try {
+        const MONGO_URI = process.env.MONGO_URI as string;
+
+        if (!MONGO_URI) {
+            throw new Error("MongoDB URI is missing");
+        }
+
+        await mongoose.connect(MONGO_URI);
+
+        console.log("MongoDB connected");
+    } catch (error) {
+    console.error("MongoDB connection error:", error);
+    process.exit(1);
+    }
+
+
+};
+
+    export default connectDB;
+
