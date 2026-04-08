abd453fb992028c0e819c9f390f549c3bdde8bf3
Joseph <blazej.sabisz@cyfroweszkoly.pl>
2026-04-08 19:29:01 +0200

    stworzenie Character.ts

--- src/models/Character.ts
+++ src/models/Character.ts
@@ -1,1 +1,28 @@
+import mongoose, { Document, Types } from "mongoose";
+
+export interface Character extends Document {
+    _id: Types.ObjectId;
+    name: string;
+    description: string;
+    species: string;
+    isHibernating: boolean;
+    bestFriend?: Types.ObjectId;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+const CharacterSchema = new mongoose.Schema(
+    {
+        name: { type: String, required: true },
+        description: { type: String, required: true },
+        species: { type: String, required: true },
+        isHibernating: { type: Boolean, required: true },
+        bestFriend: { type: Types.ObjectId, ref: "Character", required: false },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+export default mongoose.model<Character>("Character", CharacterSchema);
 
